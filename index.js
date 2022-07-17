5956e958c538615a344cfe9c9606cae360fd2e3d
PoJop <kostetzkiy.oleg@gmail.com>
2022-07-17 23:03:18 +0300

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"],
+        credentials: true
+      }
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -14,8 +20,8 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(5000, () => {
+    console.log('listening on *:5000');
 });
 io.on('connection', (socket) => {
     console.log('a user connected');
